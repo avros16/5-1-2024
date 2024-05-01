b5c28a77c1fb1bbb1581c5f81916ad98f7325d4e
avros16 <114757828+avros16@users.noreply.github.com>
2024-05-01 12:12:56 +0000

    pass by value

--- .ccls-cache/@home@runner@51/main.cpp
+++ .ccls-cache/@home@runner@51/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+
+// pass by value and pass by refernce 
+// pass by value - when you define a variable in the main function and send it to a function at the top of the program, this is called a pass by value. The values and variables are COPIED, so they appear in the memory twice. If you update the value in the function, it will not change the values within main. 
+
+
+
+
+
 int main() {
+
+
+  
   std::cout << "Hello World!\n";
+
+  
 }
