e1c1fb1e9783198f46e477d73a4f8df13321b10e
avros16 <114757828+avros16@users.noreply.github.com>
2024-05-01 12:55:54 +0000

    ex 2 pass by reference

--- .ccls-cache/@home@runner@51/main.cpp
+++ .ccls-cache/@home@runner@51/main.cpp
@@ -2,25 +2,22 @@
 using namespace std; 
 
 
-int swap(int a, int b){
+void swap(int& a, int& b){
   int e{0};
   e=b;
   b=a;
   a=e;
 
-  return a;
-  return b;
+ 
 }
 
 int main() {
 
 int a{5};
 int b{7};
-int c{0};
 
-c=b;
-b=a;
-a=c; 
+swap(a,b);
 
+  cout << a << " " << b; 
   
 }
